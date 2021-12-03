6d1df90adb1a0ac9816893479f1e36f4907c6ddc
Dwight <dwight@Marketing-1-iMac.local>
2021-12-03 16:46:12 -0500

    first commit

---
+++ src/components/MoviesList.js
@@ -0,0 +1,40 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const api_key = "8960c0881dc3cac8c7615abbfbb25548";
+const BASE_URL = "https://api.themoviedb.org/3";
+const getImage = (path) => `https://image.tmdb.org/t/p/w300/${path}`;
+
+function MovieList() {
+  const [data, setData] = useState([]);
+
+  const api = axios.create({ baseURL: BASE_URL });
+
+  const getUpcoming = api.get("movie/upcoming", { params: { api_key } });
+
+  useEffect(() => {
+    getUpcoming.then((res) => {
+      setData(res.data.results);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="grid">
+          {data.map((movie) => (
+            <div className="item">
+              <img src={getImage(movie.poster_path)} />
+              <p>{movie.original_title}</p>
+            </div>
+          ))}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default MovieList;
