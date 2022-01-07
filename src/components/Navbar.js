344c364ba7bffe85e22e4a7157fc41862a814054
Dwight Carter <bigbrother@dwights-mbp.home>
2022-01-06 19:40:25 -0500

    added trailers for netflix original

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+
 
 function Navbar() {
+
+const [offset, setOffset] = useState();
+
+  useEffect(() => {
+    window.onscroll = () => {
+      setOffset(window.pageYOffset > 100)
+    }
+  }, []);
+
+  console.log(offset); 
+
     return (
-        <div>
+        <div className={offset ? "nav_fade" : "nav"}>
             <img className="nav_logo" src="https://www.freepnglogos.com/uploads/netflix-logo-text-emblem-31.png" alt="Netflix Logo"/>
             <img className="nav_avatar" src="https://i.imgur.com/fXJosY6.jpeg" alt="Avatar"/>
-            <img />
         </div>
     )
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,12 @@
 import React,{useState, useEffect} from 'react'
 import axios  from '../axios';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 function Row({title, fetchUrl,isLargeRow}) {
 
-    const [movies,setMovies] = useState([])
+    const [movies,setMovies] = useState([]);
+    const [trailerUrl,setTrailerUrl] = useState("");
     const BASE_URL = "https://image.tmdb.org/t/p/w300";
 
 
@@ -16,25 +19,47 @@ function Row({title, fetchUrl,isLargeRow}) {
         fetchData()
     }, [fetchUrl]);
 
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+        },
+    }
+
+    const handleClick = (movie) => {
+        if(trailerUrl){
+            setTrailerUrl("");
+        }else{
+            movieTrailer(movie?.name || "")
+            .then((url) => {
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get("v"))
+            })
+            .catch((error) => console.log(error));
+        }
+        console.log(setTrailerUrl)
+
+    }
+
+
     return (
         <div className="wrapper">
         <h1 className="genre_title">{title}</h1>
 
           <div className="movie-posters">
             {movies.map((movie) => (
-                <>
                 <img
                 key={movie.id}
                 className="movie-poster"
                 alt=""
+                onClick={()=> handleClick(movie)}
                 src={`${BASE_URL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                 />
-                <div className="movie-titles">
-                {/* <h1 className="movie-title">{movie.original_title}</h1> */}
-                </div>
-                </>
             ))}
           </div>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/>}
       </div>
     )
 }
